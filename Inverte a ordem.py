6c36b9f1c160e939e4ec7f52ebb0c94c2b234b19
Nuel631 <nuelrr631@gmail.com>
2017-06-20 10:38:04 -0300

    Add files via upload

---
+++ Inverte a ordem.py
@@ -0,0 +1,14 @@
+#Programa inverte a ordem do que você digita.
+
+v=[]
+vn=[]
+n = 0
+while n < 10:
+    v.append(int(input("Digite o %dº numero : "%(n+1))))
+    n = n +1
+n = 9
+while n > -1 :
+    vn.append(v[n])
+    n = n -1
+print(v)
+print(vn)
