e536981f31edf82b27e1cdef91fd952715d4925b
Nuel631 <nuelrr631@gmail.com>
2017-06-20 09:27:18 -0300

    Add files via upload

---
+++ Conbustivel gasto.py
@@ -0,0 +1,6 @@
+#Programa informa o consumo do veiculo
+
+km=int(input("Digite quantos quilometros foram percorridos ?"))
+L=int(input("Quantos litros foram gastos ?"))
+C=km/L
+print("O consumo é de",C,"km/l")
