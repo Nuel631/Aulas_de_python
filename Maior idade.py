1ed4243289cdd52cd2fcf74f98afa6a9f75d0e13
Nuel631 <nuelrr631@gmail.com>
2017-06-22 11:02:34 -0300

    Add files via upload

---
+++ Maior idade.py
@@ -0,0 +1,20 @@
+#Programa printa maior idade escrita.
+
+m=0
+
+i1=int(input("Digite uma idade: "))
+if(i1>m):
+    m=i1
+i2=int(input("Digite uma idade: "))
+if(i2>m):
+    m=i2
+i3=int(input("Digite uma idade: "))
+if(i3>m):
+    m=i3
+i4=int(input("Digite uma idade: "))
+if(i4>m):
+    m=i4
+i5=int(input("Digite uma idade: "))
+if(i5>m):
+    m=i5
+print(m)
